2c82d2df208d0bbf556efbb374418dc87491f338
StefanosINC <Stefanos26Sophocleous@gmail.com>
2022-04-29 14:56:21 -0700

    Add new variable for user for name

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 // query selector all gets all of them
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-
+const test = 3;
 const openModal = function(){
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
@@ -15,13 +15,11 @@ const closeModal = function() {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
-
+// anonymous function -> 
+// individual click access to each 
 for(let i = 0; i < btnsOpenModal.length; i++){
-    // anonymous function -> 
-    // individual click access to each 
-   btnsOpenModal[i].addEventListener('click', openModal)
+ btnsOpenModal[i].addEventListener('click', openModal)
 }
-
     // this is way it gets called on time
     btnCloseModal.addEventListener('click', closeModal);
 
